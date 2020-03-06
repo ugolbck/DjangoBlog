001449eea88e6ab0d4f32a6709f78a52eb02ea45
Ugo Loobuyck <ugo813@hotmail.fr>
2020-03-06 20:36:49 +0100

    Added a TODO

--- showcase/urls.py
+++ showcase/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('projects/', views.proj_all, name='proj_all'),
-    path('projects/<int:article_id>/', views.proj_single, name='proj_single'), #Change article_id to article_slug
+    path('projects/<int:article_id>/', views.proj_single, name='projects'), #Change article_id to article_slug
     path('CV/', views.cvDisplay, name='cvDisplay')
 ]
--- showcase/views.py
+++ showcase/views.py
@@ -7,13 +7,15 @@ def index(request):
     latest_articles = Article.objects.order_by('-date')[:3]
     return render(request, 'showcase/index.html', {'latest_articles': latest_articles})
 
-def proj_all(request):
-    all_articles = Article.objects.all()
-    return render(request, 'showcase/detail.html', {'all_articles': all_articles})
+# TODO
+# Add projects.html and display all articles
+# Split project_render into single/all
+# Map urls in base.html to the right names from urls.py
 
-def proj_single(request, article_id):
+def project_render(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
-    return render(request, 'showcase/detail.html', {'article': article.body})
+    all_articles = Article.objects.all()
+    return render(request, 'showcase/detail.html', {'article': article, 'all_articles': all_articles})
 
 def cvDisplay(request):
     cv = get_object_or_404(CV, pk=1)
